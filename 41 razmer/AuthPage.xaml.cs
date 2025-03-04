d18b3049eedd5a1f51c8f0279780d19a598942ba
VI <VI@DESKTOP-VH707OM>
2025-03-04 12:56:52 +0500

    Добавьте файлы проекта.

---
+++ 41 razmer/AuthPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _41_razmer;
+
+namespace _41_razmer
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        private string _captcha;
+        private string _ValidLitters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyz1234567890";
+        private bool _isCaptched = true;
+        public AuthPage()
+        {
+            InitializeComponent();
+            TBCaptcha.Visibility = Visibility.Hidden;
+            TBlockCaptcha.Visibility = Visibility.Hidden;
+        }
+
+        private async void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            string guest = "Гость";
+            Manager.MainFrame.Navigate(new RazmerPage(guest, guest));
+
+            TBoxLogin.Text = "";
+            TBoxPassword.Text = "";
+            _isCaptched = true;
+            TBCaptcha.Text = "";
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TBoxLogin.Text;
+            string password = TBoxPassword.Text;
+
+            if (TBCaptcha.Text == _captcha)
+                _isCaptched = true;
+
+            if (!_isCaptched)
+            {
+                MessageBox.Show("Каптча введена неверно!");
+                CaptchaEnable();
+                LoginButton.IsEnabled = false;
+                await Task.Delay(10000);
+                LoginButton.IsEnabled = true;
+                return;
+            }
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Есть пустые поля");
+                CaptchaEnable();
+                return;
+            }
+
+            User user = Kuzmin41.getInstance().User.ToList().Find(p => p.UserLogin == login && p.UserPassword == password);
+            if (user != null)
+            {
+                Manager.MainFrame.Navigate(new RazmerPage(user));
+                TBoxLogin.Text = "";
+                TBoxPassword.Text = "";
+                CaptchaDisable();
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные");
+                if (TBCaptcha.IsVisible)
+                {
+                    LoginButton.IsEnabled = false;
+                    await Task.Delay(10000);
+                    LoginButton.IsEnabled = true;
+                }
+                CaptchaEnable();
+            }
+        }
+        private void CaptchaEnable()
+        {
+            _isCaptched = false;
+            TBCaptcha.Visibility = Visibility.Visible;
+            TBlockCaptcha.Visibility = Visibility.Visible;
+
+
+            Random random = new Random();
+
+            capthaOneWord.Text = Convert.ToString(_ValidLitters[random.Next(_ValidLitters.Length)]);
+            capthaTwoWord.Text = Convert.ToString(_ValidLitters[random.Next(_ValidLitters.Length)]);
+            capthaThreeWord.Text = Convert.ToString(_ValidLitters[random.Next(_ValidLitters.Length)]);
+            capthaFourWord.Text = Convert.ToString(_ValidLitters[random.Next(_ValidLitters.Length)]);
+
+
+
+            _captcha = capthaOneWord.Text + capthaTwoWord.Text + capthaThreeWord.Text + capthaFourWord.Text;
+        }
+        private void CaptchaDisable()
+        {
+            _isCaptched = true;
+            TBCaptcha.Visibility = Visibility.Hidden;
+            TBlockCaptcha.Visibility = Visibility.Hidden;
+            capthaOneWord.Text = "";
+            capthaTwoWord.Text = "";
+            capthaThreeWord.Text = "";
+            capthaFourWord.Text = "";
+            TBCaptcha.Text = "";
+        }
+    }
+}
---
+++ 41 razmer/RazmerPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41_razmer
+{
+    /// <summary>
+    /// Логика взаимодействия для RazmerPage.xaml
+    /// </summary>
+    public partial class RazmerPage : Page
+    {
+        List<Product> selectedProdList = new List<Product>();
+        List<OrderProduct> selectedOrderProducts = new List<OrderProduct>();
+        public RazmerPage(User user)
+        {
+            InitializeComponent();
+
+            TBUsername.Text = $"{user.UserSurname} {user.UserName} {user.UserPatronymic}";
+
+            switch (user.UserRole)
+            {
+                case 1:
+                    TBRole.Text = "Клиент";
+                    break;
+                case 2:
+                    TBRole.Text = "Менеджер";
+                    break;
+                case 3:
+                    TBRole.Text = "Администратор";
+                    break;
+            }
+
+            var currentProducts = Kuzmin41.getInstance().Product.ToList();
+
+            ProductListView.ItemsSource = currentProducts;
+
+            FiltrComboBox.SelectedIndex = 0;
+
+            UpdateProducts();
+        }
+        public RazmerPage(string userName, string userRole)
+        {
+            InitializeComponent();
+
+            TBUsername.Text = $"{userName}";
+
+            var currentProducts = Kuzmin41.getInstance().Product.ToList();
+
+            TBRole.Text = $"{userRole}";
+
+            ProductListView.ItemsSource = currentProducts;
+
+            FiltrComboBox.SelectedIndex = 0;
+
+            UpdateProducts();
+        }
+        public void UpdateProducts()
+        {
+            var currentAgents = Kuzmin41.getInstance().Product.ToList();
+
+            if (FiltrComboBox.SelectedIndex == 0)
+            {
+                currentAgents = currentAgents.Where(p => Convert.ToInt32(p.ProductDiscountAmount) <= 100).ToList();
+            }
+            if (FiltrComboBox.SelectedIndex == 1)
+            {
+                currentAgents = currentAgents.Where(p => Convert.ToInt32(p.ProductDiscountAmount) < 10).ToList();
+            }
+            if (FiltrComboBox.SelectedIndex == 2)
+            {
+                currentAgents = currentAgents.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 15).ToList();
+            }
+            if (FiltrComboBox.SelectedIndex == 3)
+            {
+                currentAgents = currentAgents.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) <= 100).ToList();
+            }
+
+            if (SortComboBox.IsChecked == true)
+            {
+                currentAgents = currentAgents.OrderBy(p => p.ProductCostInt).ToList();
+            }
+            if (SortComboBox1.IsChecked == true)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.ProductCostInt).ToList();
+            }
+
+            currentAgents = currentAgents.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            ProductListView.ItemsSource = currentAgents;
+            TBlockCount.Text = Convert.ToString(currentAgents.Count());
+        }
+
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void FiltrComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void SortComboBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+        private void SortComboBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void ProductListView_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+
+            if (item != null)
+            {
+                (sender as ListView).ContextMenu.IsOpen = true;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedProduct = ProductListView.SelectedItem as Product;
+            if (selectedProduct != null && selectedProduct.ProductQuantityInStock <= 1)
+            {
+                selectedProdList.Add(selectedProduct);
+            }
+            else
+            {
+                //selectedProdList
+            }
+        }
+
+        private void BtnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            //OrderWindow window = new OrderWindow(selectedProdList, selectedOrderList,user);
+            //bool? result = window.ShowDialog();
+            //if (result == true)
+            //{
+            //    UpdateProducts();
+            //}
+            //else
+            //{
+            //    UpdateProducts();
+            //}
+        }
+    }
+}
